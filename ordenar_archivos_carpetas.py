2a55e967172b4f1ab2882044cfa56ad9ffb2f4ac
fguedez1311 <46085315+fguedez1311@users.noreply.github.com>
2025-05-04 19:59:03 -0400

    función para crear carpetas

--- ordenar_archivos_carpetas.py
+++ ordenar_archivos_carpetas.py
@@ -1,28 +1,37 @@
 import os
 import shutil
+from tkinter import filedialog, Tk
 
 #Rutas donde estas los archivos y carpetas
-ruta_archivos = "C:/Users/PC/Documents"
+# ruta_archivos = "C:/Users/PC/Documents"
 # Crear carpetas en destino sino existen
-tipos=["Imagenes","PDFs","Videos","Documentos_Word","Documentos_Excel"]
+# tipos=["Imagenes","PDFs","Videos","Documentos_Word","Documentos_Excel"]
+extensiones={
+    ".jpg":"Imagenes",
+    ".png":"Imagenes",
+    ".pdf":"PDFs",
+    ".mp4":"Videos",
+    ".docx":"Documentos_Word",
+    ".xlsx":"Documentos_Excel",
+    ".xls":"Documentos_Excel",
+}
+Ventana = Tk()
+Ventana.withdraw()
+ruta_archivos=filedialog.askdirectory(title="Selecciona la carpeta donde están los archivos")
+for carpeta in set(extensiones.values()):
+        ruta_carpeta = os.path.join(ruta_archivos, carpeta)
+        if not os.path.exists(ruta_carpeta):
+            os.makedirs(ruta_carpeta)
 
-for carpeta in tipos:
-        if not os.path.exists(os.path.join(ruta_archivos,carpeta)):
-            os.makedirs(os.path.join(ruta_archivos,carpeta))
-        
-# Obtener la lista de archivos en la ruta
-for archivo in os.listdir(ruta_archivos):
-    # Verificar si es un archivo
-    if os.path.isfile(os.path.join(ruta_archivos, archivo)):
-        # Obtener la extensión del archivo
-        extension = archivo.split(".")[-1].lower()
-        
-        # Mover el archivo a la carpeta correspondiente según su extensión
-        if extension in ["jpg", "jpeg", "png", "gif"]:
-            shutil.move(os.path.join(ruta_archivos, archivo), os.path.join(ruta_archivos, "Imagenes", archivo))
-        elif extension in ["pdf"]:
-            shutil.move(os.path.join(ruta_archivos, archivo), os.path.join(ruta_archivos, "PDFs", archivo))
-        elif extension in ["docx"  , "doc"]:
-            shutil.move(os.path.join(ruta_archivos, archivo), os.path.join(ruta_archivos, "Documentos_Word", archivo))
-        elif extension in ["xlsx"  , "xls"]:
-            shutil.move(os.path.join(ruta_archivos, archivo), os.path.join(ruta_archivos, "Documentos_Excel", archivo))
+for  archivo in os.listdir(ruta_archivos):
+    ruta_archivo = os.path.join(ruta_archivos, archivo)
+    if os.path.isfile(ruta_archivo):
+        _, extension = os.path.splitext(archivo)
+        extension = extension.lower()
+        if extension in extensiones:
+            carpeta_destino = extensiones[extension]
+            ruta_destino = os.path.join(ruta_archivos, carpeta_destino, archivo)
+            shutil.move(ruta_archivo, ruta_destino)
+            print(f"Moviendo {archivo} a {carpeta_destino}")
+        else:
+            print(f"No se encontró la extensión de {archivo}")
